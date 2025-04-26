7a412007e18f4af7a0984850f06cd1d861bdc35a
Anatoly <anatolyavbme@gmail.com>
2025-04-26 13:25:16 +0300

    chore: add global exception handler and minor improvements

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
 using CustomerApi.Data;
+using CustomerApi.Middleware; 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Diagnostics;
-using System.Net;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -17,34 +14,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseExceptionHandler(errorApp =>
-{
-    errorApp.Run(async context =>
-    {
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        context.Response.ContentType = "application/json";
 
-        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
-
-        if (exceptionHandlerPathFeature?.Error != null)
-        {
-            var errorResponse = new
-            {
-                error = exceptionHandlerPathFeature.Error.Message
-            };
-
-            var errorJson = JsonSerializer.Serialize(errorResponse);
-            await context.Response.WriteAsync(errorJson);
-        }
-    });
-});
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
--- obj/Debug/net9.0/CustomerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/CustomerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf9b8c36e6c5f324d6455e0488280abb50aaf39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870c8994df10b5fda137d6014fb920802b1f12aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
