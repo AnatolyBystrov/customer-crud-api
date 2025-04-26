2a15b5fb58f27b0a8ceb1ef44301e53d96fb6f71
Anatoly <anatolyavbme@gmail.com>
2025-04-26 16:03:34 +0300

    fix: listen on PORT for Render deployment

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 using CustomerApi.Data;
-using CustomerApi.Middleware; 
+using CustomerApi.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -15,12 +14,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
